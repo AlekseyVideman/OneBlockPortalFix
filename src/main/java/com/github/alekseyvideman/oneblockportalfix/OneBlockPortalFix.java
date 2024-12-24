a8afcc2534bc1159f9c23987ebe81d8eda5f49cb
Aleksey Videman <alekseyvideman@gmail.com>
2024-12-24 22:03:30 +0700

    Init

---
+++ src/main/java/com/github/alekseyvideman/oneblockportalfix/OneBlockPortalFix.java
@@ -0,0 +1,20 @@
+package com.github.alekseyvideman.oneblockportalfix;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OneBlockPortalFix extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void on(PlayerPortalEvent e) {
+        if (e.isCancelled()) e.setCancelled(false);
+    }
+}
